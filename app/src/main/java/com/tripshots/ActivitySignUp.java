3b8d971e62b7a33d5e087c2c6c1e735d4452b8b8
itnug987 <srinivasgunti987@gmail.com>
2020-04-25 11:03:50 +0530

    Added user information in sign up using Firebase real time database

--- app/src/main/java/com/tripshots/ActivitySignUp.java
+++ app/src/main/java/com/tripshots/ActivitySignUp.java
@@ -13,11 +13,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tripshots.Data.sharedPref;
+import com.tripshots.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivitySignUp extends AppCompatActivity {
 
@@ -26,6 +33,8 @@ public class ActivitySignUp extends AppCompatActivity {
     sharedPref sharedPref;
 
     Button btn_already_registered;
+
+    EditText name, location;
 
     private FirebaseAuth mAuth;
 
@@ -38,35 +47,55 @@ public class ActivitySignUp extends AppCompatActivity {
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        name = findViewById(R.id.name);
+        location = findViewById(R.id.location);
+
         btn_register = findViewById(R.id.btn_register);
 
         btn_already_registered = findViewById(R.id.btn_already_registered);
 
         mAuth = FirebaseAuth.getInstance();
 
-
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_register.setBackground(getDrawable(R.drawable.rounded_button_after));
-                if(email.getText().length()!=0 && password.getText().length()!=0) {
+
                     mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                             .addOnCompleteListener(ActivitySignUp.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         sharedPref.createLoginSession();
-                                        Intent i = new Intent(ActivitySignUp.this, MainActivity.class);
-                                        startActivity(i);
+
+                                        User user = new User(name.getText().toString(), email.getText().toString(),
+                                                 location.getText().toString());
+
+                                        FirebaseDatabase.getInstance().getReference("users")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    Toast.makeText(ActivitySignUp.this,
+                                                            "Registration Success", Toast.LENGTH_SHORT).show();
+                                                    Intent i = new Intent(ActivitySignUp.this, MainActivity.class);
+                                                    startActivity(i);
+                                                }else{
+                                                    Toast.makeText(ActivitySignUp.this,
+                                                            "Registration Not Success", Toast.LENGTH_SHORT).show();
+
+                                                }
+                                            }
+                                        });
+
+
                                     } else {
                                         Toast.makeText(ActivitySignUp.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-                }
-                else
-                    Toast.makeText(ActivitySignUp.this, "Please enter Email and Password", Toast.LENGTH_SHORT).show();
-            }
+              }
         });
 
         btn_already_registered.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/tripshots/model/User.java
@@ -0,0 +1,16 @@
+package com.tripshots.model;
+
+public class User {
+
+    public String name, email, location;
+
+    public User(){
+
+    }
+
+    public User(String name, String email,  String location) {
+        this.name = name;
+        this.email = email;
+        this.location = location;
+    }
+}
