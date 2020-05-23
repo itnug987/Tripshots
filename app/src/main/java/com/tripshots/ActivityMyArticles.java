84a389cd2c6f2637da9b8e36942caeb4e12bb39c
itnug987 <srinivasgunti987@gmail.com>
2020-05-23 20:35:47 +0530

    Added my articles page

--- app/src/main/java/com/tripshots/ActivityMyArticles.java
+++ app/src/main/java/com/tripshots/ActivityMyArticles.java
@@ -12,16 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,25 +27,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.tripshots.Adapter.PostAdapter;
-import com.tripshots.Adapter.blogAdapter;
-import com.tripshots.Api.api;
 import com.tripshots.Data.sharedPref;
 import com.tripshots.model.Post;
-import com.tripshots.response.blog_item;
-import com.tripshots.response.blog_response;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-public class MainActivity extends AppCompatActivity {
-
+public class ActivityMyArticles extends AppCompatActivity {
     private ActionBar actionBar;
     private Toolbar toolbar;
     private NavigationView nav_view;
@@ -56,52 +41,33 @@ public class MainActivity extends AppCompatActivity {
     com.tripshots.Data.sharedPref sharedPref;
 
     RecyclerView blog_recycler_view;
-    RecyclerView blog_firebase_recycler_view;
     ProgressBar progressBar;
 
     TextView drawer_name, drawer_location;
 
-    FloatingActionButton add_post;
-
-    Retrofit.Builder builder = new Retrofit.Builder()
-            .baseUrl(com.tripshots.Api.api.url)
-            .addConverterFactory(GsonConverterFactory.create());
-    Retrofit retrofit = builder.build();
-    com.tripshots.Api.api Api = retrofit.create(com.tripshots.Api.api.class);
-
     List<Post> postList;
     PostAdapter postAdapter;
 
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my_articles);
 
         initToolbar();
         initDrawerMenu();
 
-        sharedPref = new sharedPref(getApplicationContext());
-
-        HashMap<String, String> user = sharedPref.getUserDetails();
-
-        final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         blog_recycler_view = findViewById(R.id.blog_items);
+
         blog_recycler_view.setLayoutManager(new LinearLayoutManager(this));
-
-        blog_firebase_recycler_view = findViewById(R.id.blog_items_firebase);
-        blog_firebase_recycler_view.setLayoutManager(new LinearLayoutManager(this));
-
-        add_post = findViewById(R.id.add_post);
 
         progressBar = findViewById(R.id.progress_bar);
         progressBar.setVisibility(View.VISIBLE);
 
         getBlogPosts();
-
-        getBlogPostsFirebase();
-
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference().child("users").child(uid);
@@ -131,15 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        add_post.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, ActivityAddPost.class);
-                startActivity(i);
-            }
-        });
-
     }
 
     private void initToolbar() {
@@ -182,18 +139,18 @@ public class MainActivity extends AppCompatActivity {
 
         switch (id) {
             case R.id.nav_home:
-                i = new Intent(MainActivity.this, MainActivity.class);
+                i = new Intent(ActivityMyArticles.this, MainActivity.class);
                 startActivity(i);
                 break;
 
             case R.id.nav_my_articles:
-                i = new Intent(MainActivity.this, ActivityMyArticles.class);
+                i = new Intent(ActivityMyArticles.this, ActivityMyArticles.class);
                 startActivity(i);
                 break;
 
             case R.id.nav_logout:
                 sharedPref.logoutUser();
-                i = new Intent(MainActivity.this, ActivitySignUp.class);
+                i = new Intent(ActivityMyArticles.this, ActivitySignUp.class);
                 startActivity(i);
                 break;
 
@@ -216,61 +173,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getBlogPosts(){
-        Call<blog_response> call = Api.getBlogPosts("AIzaSyBdlfcNJoSNNgbhjnzv2SsB3tPEz7Qkh8Q");
-
-        call.enqueue(new Callback<blog_response>() {
-            @Override
-            public void onResponse(Call<blog_response> call, Response<blog_response> response) {
-                if(response.isSuccessful()){
-                    progressBar.setVisibility(View.GONE);
-                    Log.d("blog retrieval", "success");
-                    blog_response blogResponse = response.body();
-
-                    List<blog_item> result = blogResponse.getItems();
-                    blog_recycler_view.setAdapter(new blogAdapter(MainActivity.this, blogResponse.getItems()));
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<blog_response> call, Throwable t) {
-                Log.d("Blog retrieval failed", t.getCause().toString());
-                Log.d("blog retrieval", "failure");
-
-
-            }
-        });
-    }
-
-    private void getBlogPostsFirebase(){
         final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("posts");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                progressBar.setVisibility(View.GONE);
+                
                 postList = new ArrayList<>();
 
-                Log.d("firebase posts", "message");
+                Iterable<DataSnapshot> postChildren = dataSnapshot.child(uid).getChildren();
 
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                for (DataSnapshot postSnap : postChildren) {
 
-                    Log.d("dataSnapshot key", FirebaseAuth.getInstance().getUid());
+                    Post post = postSnap.getValue(Post.class);
 
-                    Iterable<DataSnapshot> postChildren = snapshot.getChildren();
+                    Log.d("post title", post.getTitle());
 
-                    for (DataSnapshot postSnap : postChildren) {
-
-                        Post post = postSnap.getValue(Post.class);
-
-                        Log.d("post title", post.getTitle());
-
-                        postList.add(post);
-                    }
-
-                    postAdapter = new PostAdapter(MainActivity.this, postList);
-                    blog_firebase_recycler_view.setAdapter(postAdapter);
-
+                    postList.add(post);
                 }
+
+                postAdapter = new PostAdapter(ActivityMyArticles.this, postList);
+                blog_recycler_view.setAdapter(postAdapter);
+
             }
 
             @Override
@@ -278,12 +204,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
 
     public void showDialogAbout() {
         AlertDialog.Builder alertBuilder;
-        alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertBuilder = new AlertDialog.Builder(ActivityMyArticles.this);
 
         alertBuilder.setMessage(R.string.about_us)
                 .setCancelable(false)
@@ -302,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
     public void showDialogSupport(){
         AlertDialog.Builder alertBuilder;
 
-        alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertBuilder = new AlertDialog.Builder(ActivityMyArticles.this);
 
         alertBuilder.setMessage(R.string.support)
                 .setCancelable(false)
